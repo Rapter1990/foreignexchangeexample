0552cf44b7a47ad8511c61c967d22e4243980681
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2024-08-09 19:01:10 +0300

    Task 6 : Add Java Doc for ForeignexchangeexampleApplication

--- src/main/java/com/example/demo/ForeignexchangeexampleApplication.java
+++ src/main/java/com/example/demo/ForeignexchangeexampleApplication.java
@@ -3,9 +3,17 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main class named {@link ForeignexchangeexampleApplication} to start the Role Permission example application.
+ */
 @SpringBootApplication
 public class ForeignexchangeexampleApplication {
 
+	/**
+	 * Main method to start the Spring Boot application.
+	 *
+	 * @param args the command line arguments
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(ForeignexchangeexampleApplication.class, args);
 	}
